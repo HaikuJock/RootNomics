ec92df9f2837869ba1a05ca6fecd7eae21b3db3f
jockf <jockfindlay@gmail.com>
2023-02-04 18:55:10 +0000

    Simulation: Add EconomySim from https://github.com/Vibr8gKiwi/bazaarBot2, changed project to a class library, dropped the form
    Newtonsoft is an incompatible version.
    Compiles OK.

---
+++ RootNomicsGame/Simulation/RootNomicEconomy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RootNomicsGame.Simulation
+{
+    internal class RootNomicEconomy
+    {
+        // subclass Economy
+        //public virtual void signalBankrupt(Market m, BasicAgent a)
+        //{
+        //    //no implemenation -- provide your own in a subclass
+        //}
+    }
+}

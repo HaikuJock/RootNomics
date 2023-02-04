2d04a1349c6d8c516ed5d4a4efece3156545179a
jockf <jockfindlay@gmail.com>
2023-02-04 10:27:50 +0000

    Simulation: Initial classes

---
+++ RootNomicsGame/Simulation/Agent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RootNomicsGame.Simulation
+{
+    internal struct Agent
+    {
+        public string Id;   // Unique
+        public string Type; // Of a set
+        public float Wealth;    // Range is TBD
+    }
+}
---
+++ RootNomicsGame/Simulation/Simulator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RootNomicsGame.Simulation
+{
+    internal class Simulator
+    {
+        internal void Initialize(IDictionary<string, string> agentTypeCount)
+        {
+
+        }
+
+        internal IEnumerable<Agent> Simulate(IDictionary<string, string> agentTypeCount)
+        {
+            return new List<Agent>();
+        }
+    }
+}
